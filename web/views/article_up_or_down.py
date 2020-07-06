b2e2d702df99d47a2c03b5f53c57d6b84e393e45
xxt123456 <1264069785@qq.com>
2020-07-06 18:12:59 +0800

    添加搜索

--- web/views/article_up_or_down.py
+++ web/views/article_up_or_down.py
@@ -15,7 +15,7 @@ def artilce_give(request):
     article_id = request.POST.get('article_id')
     up_or_down = bool(int(request.POST.get('up_or_down')))
     up_or_down_id = models.UpDown.objects.filter(user_id=user_id, article_id=article_id)
-    ret = {'status': None, 'message': None}
+    ret = {'status': None, 'message': None, 'count_num': None}
     if not up_or_down_id:
         # 不存在点赞记录进行记录创建
         try:
@@ -25,13 +25,16 @@ def artilce_give(request):
                 from django.db.models import F
                 if up_or_down == True:
                     models.Article.objects.select_for_update().filter(nid=article_id).update(up_count=F('up_count') + 1)
-                    ####传值
-                    print(models.Article.objects.filter(nid=article_id).values(up_or_down=F('up_count')))
-                    ret = {'status': 1, 'message': '点赞成功'}
+                    ####传点赞数
+                    count = list(models.Article.objects.filter(nid=article_id).values(up_or_down=F('up_count')))
+                    count_num = count[0]['up_or_down']
+                    ret = {'status': 1, 'message': '点赞成功', 'count_num': count_num}
                 elif up_or_down == False:
                     models.Article.objects.select_for_update().filter(nid=article_id).update(
                         down_count=F('down_count') + 1)
-                    ret = {'status': 2, 'message': '踩赞成功'}
+                    count = list(models.Article.objects.filter(nid=article_id).values(up_or_down=F('down_count')))
+                    count_num = count[0]['up_or_down']
+                    ret = {'status': 2, 'message': '踩赞成功', 'count_num': count_num}
         except Exception as e:
             print('2222', e)
     if up_or_down_id:
@@ -52,13 +55,17 @@ def artilce_give(request):
             if up_or_down == True and uo_or_down_stauts['up'] == True:
                 models.Article.objects.select_for_update().filter(nid=article_id).update(up_count=F('up_count') - 1)
                 models.UpDown.objects.select_for_update().filter(user_id=user_id, article_id=article_id).delete()
-                ret = {'status': 3, 'message': '取消点赞'}
+                count = list(models.Article.objects.filter(nid=article_id).values(up_or_down=F('up_count')))
+                count_num = count[0]['up_or_down']
+                ret = {'status': 3, 'message': '取消点赞', 'count_num': count_num}
             if up_or_down == True and uo_or_down_stauts['up'] == False:
                 ret = {'status': 4, 'message': '已踩赞，请勿点赞'}
             if up_or_down == False and uo_or_down_stauts['up'] == False:
                 models.Article.objects.select_for_update().filter(nid=article_id).update(down_count=F('down_count') - 1)
                 models.UpDown.objects.select_for_update().filter(user_id=user_id, article_id=article_id).delete()
-                ret = {'status': 5, 'message': '取消踩暂'}
+                count = list(models.Article.objects.filter(nid=article_id).values(up_or_down=F('down_count')))
+                count_num = count[0]['up_or_down']
+                ret = {'status': 5, 'message': '取消踩暂', 'count_num': count_num}
             if up_or_down == False and uo_or_down_stauts['up'] == True:
                 ret = {'status': 6, 'message': '已点赞，请勿踩赞'}
         except Exception as e:
