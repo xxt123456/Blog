c29418df17e2452871adae1c18e85d28bac25170
xxt123456 <1264069785@qq.com>
2020-07-16 20:07:14 +0800

    修复微博爬虫逻辑问题

--- backend/views/sprider_weibo.py
+++ backend/views/sprider_weibo.py
@@ -23,28 +23,63 @@ time.sleep(8)
 # s=ss.find_all(name='div',attrs={'id':'plc_unlogin_home_main'})
 # s = ss.find_all('ul')
 weibo_pages = bf(browser.page_source, 'lxml')
-weibo_page = weibo_pages.find_all(name='div', attrs={'id': 'plc_unlogin_home_main'})
+weibo_page = weibo_pages.find_all(name='div', attrs={'id': 'pl_unlogin_home_feed'})
 for title in weibo_page:
-    # title_list = title.find_all(name='ul', attrs={'node-type': 'feed_list'})
-    title_list = title.find_all(name='ul', attrs={'action-type': 'feed_list'})
-    for obj in title_list:
-        video = obj.find(attrs={'node-type': re.compile(r'fl_h5_video$')})
-        list_imgs = obj.find('div', attrs={'class': 'UG_list_a'})
-        # 微博内容为视频时
-        if video:
-            img = video.find('img').get('src')  # 视频照片地址
+    # title_list = weibo_page.find_all(name='ul', attrs={'node-type': 'feed_list'})
+    # title_list = weibo_page.find_all(name='div', attrs={'class': 'UG_list_v2 clearfix'})
+    list_imgA = title.find_all('div', attrs={'action-type': 'feed_list_item', 'class': 'UG_list_a'})
+    list_imgB = title.find_all('div', attrs={'action-type': 'feed_list_item', 'class': 'UG_list_b'})
+    list_imgC = title.find_all('div', attrs={'action-type': 'feed_list_item', 'class': 'UG_list_c'})
+    print('1111111111', title)
+    # print('1111111112222',title_listA)
+    print('11AAAAAAA2', list_imgA)
+    print('11BBBBBBB2222', list_imgB)
+    print('11CCCCCCC2222', list_imgC)
 
-        if list_imgs:
-            title = obj.find('h3').text  # 博文主题
-            imgs = obj.findAll('div', attrs={'class': 'list_nod clearfix'})
+    # for obj in title_list:
+    #     print('333333333',obj)
+    #     # print('2222',obj.findAll('div',attrs={'action-type': 'feed_list_item'}))
+    #     video = obj.find(attrs={'node-type': re.compile(r'fl_h5_video$')})
+    #     list_imgA = obj.find('div',attrs={'class': 'UG_list_a'})
+    #     list_imgB = obj.find('div',attrs={'class': 'UG_list_b'})
+    #     list_imgC = obj.find('div',attrs={'class': 'UG_list_c'})
+    #     print('1111111',video)
+    #     print('1333333',list_imgA)
+    #     print('122222',list_imgA)
+    #     print('144444444',list_imgA)
+    # # 微博内容为视频时
+    # if video:
+    #     img = video.find('img').get('src')  # 视频照片地址
+    #     href = obj.find('h3').find('a', attrs={'extra-data': 'type=topic'})  # 博文链接
+    #     face = obj.find('span', attrs={'class': 'subinfo_face'}).find('img').get('src')  # 博主头像
+
+    if list_imgA:
+        for list_img in list_imgA:
+            # title = obj.find('h3').text  # 博文主题
+            imgs = list_img.findAll('div', attrs={'class': 'list_nod clearfix'})
+            href = list_img.find('h3').find('a', attrs={'extra-data': 'type=topic'})  # 博文链接
+            face = list_img.find('span', attrs={'class': 'subinfo_face'}).find('img').get('src')  # 博主头像
             for img in imgs:
+                print(imgs)
                 img = img.find('img').get('src')  # 微博照片
-        title = obj.find('h3').text  # 博文主题
-        href = obj.find('h3').find('a', attrs={'extra-data': 'type=topic'})  # 博文链接
-        face = obj.find('span', attrs={'class': 'subinfo_face'}).find('img').get('src')  # 博主头像
-        username = obj.find('span', attrs={'class': 'subinfo S_txt2'}).text  # 博主昵称
+                print('AAA', img)
+    if list_imgB:
+        for list_img in list_imgB:
+            img = list_img.find('div', attrs={'class': 'pic W_piccut_v'}).find('img').get('src')
+            print('BBBB', img)
 
-        print(video)
-        print(title, username)
+    if list_imgC:
+        for list_img in list_imgC:
+            img = list_img.find('div', attrs={'class': 'pic W_piccut_v'}).find('img').get('src')
+            print('CCCC', img)
+    title = title.find('h3').text  # 博文主题
+    # try:
+    #     username = title.find('span', attrs={'class': 'subinfo S_txt2'}).text  # 博主昵称
+    # except AttributeError as e:
+    #     pass
+    # print(e)
+
+    # print(username)
+
 
 browser.quit()
