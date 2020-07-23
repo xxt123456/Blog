8720293c34d201d77587236a2bc78e2ab652d5fc
xxt123456 <1264069785@qq.com>
2020-07-23 20:08:36 +0800

    增加微博搜索爬虫

--- backend/views/sprider_weibo.py
+++ backend/views/sprider_weibo.py
@@ -4,12 +4,15 @@ import re
 from selenium import webdriver
 from bs4 import BeautifulSoup as bf
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-browser = webdriver.Chrome(chrome_options=chrome_options)
-# browser=webdriver.Chrome()
-browser.get('https://weibo.com/')
-time.sleep(8)
+
+def Browser_Driver():
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument('--headless')
+    # browser = webdriver.Chrome(chrome_options=chrome_options)
+    browser = webdriver.Chrome()
+    browser.get('https://weibo.com/')
+    time.sleep(8)
+    return browser
 
 
 # weibo_page = requests.get(url='https://weibo.com/',
@@ -22,58 +25,117 @@ time.sleep(8)
 # ss = bf(weibo_page.text, 'lxml')
 # s=ss.find_all(name='div',attrs={'id':'plc_unlogin_home_main'})
 # s = ss.find_all('ul')
-weibo_pages = bf(browser.page_source, 'lxml')
-weibo_page = weibo_pages.find_all(name='div', attrs={'id': 'pl_unlogin_home_feed'})
-for title in weibo_page:
-    # title_list = weibo_page.find_all(name='ul', attrs={'node-type': 'feed_list'})
-    # title_list = weibo_page.find_all(name='div', attrs={'class': 'UG_list_v2 clearfix'})
-    list_imgA = title.find_all('div', attrs={'action-type': 'feed_list_item', 'class': 'UG_list_a'})
-    list_imgB = title.find_all('div', attrs={'action-type': 'feed_list_item', 'class': 'UG_list_b'})
-    list_imgC = title.find_all('div', attrs={'action-type': 'feed_list_item', 'class': 'UG_list_c'})
-    videos = title.find_all('div', attrs={'action-type': 'feed_list_item', 'class': 'UG_list_v2 clearfix'})
-    # 微博内容为视频时
-    if videos:
-        for list_video in videos:
-            img = list_video.find('img').get('src')  # 视频照片地址
-            href = list_video.find('h3').find('a', attrs={'extra-data': 'type=topic'})  # 博文链接
-            face = list_video.find('span', attrs={'class': 'subinfo_face'}).find('img').get('src')  # 博主头像
-            weibo_title = list_video.find('h3').text  # 博文主题
-            username = list_video.find('span', attrs={'class': 'subinfo S_txt2'}).text  # 博主昵称
-            print('VVV', username, img, weibo_title)
+# 获取热门分页数据
+def WeiBo_page(index, *args):
+    browser = Browser_Driver()
+    # 通过args区分搜索关键字
+    if not args:
+        for i in range(index):
+            el = browser.find_element_by_xpath('//*[contains(text(),"正在加载中，请稍候...")]')
+            time.sleep(3)
+            browser.execute_script("arguments[0].scrollIntoView();", el)
+            time.sleep(3)
+    else:
+        key = args[0]
+        browser.find_element_by_xpath('//*[@id="weibo_top_public"]/div/div/div[2]/input').send_keys(key)
+        browser.find_element_by_xpath('//*[@id="weibo_top_public"]/div/div/div[2]/a').click()
+
+    weibo_pages = bf(browser.page_source, 'lxml')
+    return weibo_pages
 
 
-    if list_imgA:
-        for list_img in list_imgA:
-            title = list_img.find('h3').text  # 博文主题
-            imgs = list_img.find('div', attrs={'class': 'list_nod clearfix'}).findAll('img')
-            href = list_img.find('h3').find('a', attrs={'extra-data': 'type=topic'})  # 博文链接
-            face = list_img.find('span', attrs={'class': 'subinfo_face'}).find('img').get('src')  # 博主头像
-            for img in imgs:
-                img = img.get('src')  # 微博照片
-                print('AAA', img, title)
+def WeiBo_Hot(index, *args):
+    browser = WeiBo_page(index, *args)
+    if index:
+        weibo_page = browser.find_all(name='div', attrs={'id': 'pl_unlogin_home_feed'})
+    if args:
+        weibo_page = browser.find_all(name='div', attrs={'id': 'pl_feedlist_index'})
+    for title in weibo_page:
+        # title_list = weibo_page.find_all(name='ul', attrs={'node-type': 'feed_list'})
+        # title_list = weibo_page.find_all(name='div', attrs={'class': 'UG_list_v2 clearfix'})
+        list_imgA = title.find_all('div', attrs={'action-type': 'feed_list_item', 'class': 'UG_list_a'})
+        list_imgB = title.find_all('div', attrs={'action-type': 'feed_list_item', 'class': 'UG_list_b'})
+        list_imgC = title.find_all('div', attrs={'action-type': 'feed_list_item', 'class': 'UG_list_c'})
+        videos = title.find_all('div', attrs={'action-type': 'feed_list_item', 'class': 'UG_list_v2 clearfix'})
+        weibo_serach = title.find_all('div', attrs={'action-type': 'feed_list_item', 'class': 'card-wrap'})
+        # 微博热门内容为视频时
+        if videos:
+            for list_video in videos:
+                img = list_video.find('img').get('src')  # 视频照片地址
+                href = list_video.find('h3').find('a', attrs={'extra-data': 'type=topic'})  # 博文链接
+                face = list_video.find('span', attrs={'class': 'subinfo_face'}).find('img').get('src')  # 博主头像
+                weibo_title = list_video.find('h3').text  # 博文主题
+                username = list_video.find('span', attrs={'class': 'subinfo S_txt2'}).text  # 博主昵称
+                weibo_datas = list_video.find('div', attrs={'class': 'subinfo_box clearfix'}).find('a').next_siblings
+                weibo_data = []
+                for i in weibo_datas:
+                    if i == '\n':
+                        pass
+                    else:
+                        weibo_data.append(i.string.strip().replace("\n", ""))
+                weibo_data = weibo_data[1]
+
+                print('VVV', username, img, weibo_title, weibo_data)
+
+        # 微博热门展示为4张图片
+        if list_imgA:
+            for list_img in list_imgA:
+                title = list_img.find('h3').text  # 博文主题
+                imgs = list_img.find('div', attrs={'class': 'list_nod clearfix'}).findAll('img')
+                href = list_img.find('h3').find('a', attrs={'extra-data': 'type=topic'})  # 博文链接
+                face = list_img.find('span', attrs={'class': 'subinfo_face'}).find('img').get('src')  # 博主头像
+                username = list_img.find('span', attrs={'class': 'subinfo S_txt2'}).text  # 博主昵称
+                weibo_datas = list_img.find('div', attrs={'class': 'subinfo_box clearfix'}).find('a').next_siblings
+                weibo_data = []
+                for i in weibo_datas:
+                    if i == '\n':
+                        pass
+                    else:
+                        weibo_data.append(i.string)
+                weibo_data = weibo_data[1]
+                for img in imgs:
+                    img = img.get('src')  # 微博照片
+                # print('AAA', username, title)
+
+        # 微博热门展示为1张图片
+        if list_imgB:
+            for list_img in list_imgB:
+                title = list_img.find('h3').text  # 博文主题
+                img = list_img.find('div', attrs={'class': re.compile(r'pic W_piccut')}).find('img').get('src')
+                username = list_img.find('span', attrs={'class': 'subinfo S_txt2'}).text  # 博主昵称
+                weibo_datas = list_img.find('div', attrs={'class': 'subinfo_box clearfix'}).find('a').next_siblings
+                weibo_data = []
+                for i in weibo_datas:
+                    if i == '\n':
+                        pass
+                    else:
+                        weibo_data.append(i.string)
+                weibo_data = weibo_data[1]
+                # print('BBBB', username,img, title)
+        # 微博热门话题
+        # if list_imgC:
+        #     for list_img in list_imgC:
+        #         title = list_img.find('h3').text  # 博文主题
+        #         img = list_img.find('div', attrs={'class': 'pic W_piccut_v'}).find('img').get('src')
+        #         username = list_img.find('span', attrs={'class': 'subinfo S_txt2'}).text  # 博主昵称
+        #         print('CCCC', img, title)
+        # 搜索通过关键字获取的数据
+        if weibo_serach:
+            for list_img in weibo_serach:
+                nick_names = list_img.find('a', attrs={'class': 'name'})
+                if nick_names == None:
+                    pass
+                else:
+                    nick_name = nick_names.string  # 博主昵称
+                weibo_contents = list_img.find('p', attrs={'class': 'txt', 'node-type': 'feed_list_content'})
+                if weibo_contents == None:
+                    pass
+                else:
+                    weibo_content = weibo_contents.text.strip()
 
 
-    if list_imgB:
-        for list_img in list_imgB:
-            title = list_img.find('h3').text  # 博文主题
-            img = list_img.find('div', attrs={'class': re.compile(r'pic W_piccut')}).find('img').get('src')
-            # img=list_img.find()
-            print('BBBB', img, title)
-    #
-    if list_imgC:
-        for list_img in list_imgC:
-            title = list_img.find('h3').text  # 博文主题
-            img = list_img.find('div', attrs={'class': 'pic W_piccut_v'}).find('img').get('src')
-            print('CCCC', img, title)
-    # weibo_title = title.find('h3').text  # 博文主题
-    # try:
-    #     # username = title.find('span', attrs={'class': 'subinfo S_txt2'}).text  # 博主昵称
-    #     # print(username, weibo_title)
-    # except AttributeError as e:
-    #     print(e)
+# def WeiBo_Serach(key):
+#
+#     weib0_key=WeiBo_page(key).find('div',attrs={'class':' gn_search_v2'})
 
-
-
-
-
-browser.quit()
+WeiBo_Hot(2, '王')
